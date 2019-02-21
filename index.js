d09f27e6779796600676f0b6c4f8069872e4b1c4
Kristina <kristinavolk4@tut.by>
2019-02-21 15:20:27 +0000

    Init

---
+++ index.js
@@ -0,0 +1,54 @@
+import data  from './fakepeople.js';
+
+console.log(data);
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ /*for (var i = 0; i < local_data.length; i++) {
+    if (local_data[i].eyeColor === "green"){
+        var row_data = row_data + '<tr>' +
+    ' <td>' + local_data[i].name.first +'</td>' +
+    ' <td>' + local_data[i].eyeColor + '</td>' +
+    ' <td>' + local_data[i].age  +
+    '</tr>';
+    }
+}
+// var table_body_element = document.createElement('tbody');
+   // table_body_element.innerHTML = row_data;
+   // document.getElementById('my_table').appendChild(table_body_element);
