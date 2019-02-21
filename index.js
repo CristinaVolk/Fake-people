f6c027d6753c25dd2178ca8310d403955f91c8f7
Kristina <kristinavolk4@tut.by>
2019-02-21 21:20:23 +0000

    index js file is modified

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-import fakepeople  from './fakepeople.js';
+import * as fakepeople  from './fakepeople.json';
 
-console.log(fakepeople);
+console.log(fakepeople[0].age);
+var local_data = fakepeople;
+var numberOfgreen = document.getElementById("green-people");
+var greenNumber=0;
+for (var i = 0; i < local_data.length; i++) {
+    if (local_data[i].eyeColor === "green"){
+       greenNumber++;
+    }
+}
+numberOfgreen.innerHTML = greenNumber;  
 
+//Longitude of Eiffel Tower: 2.294481
+//Latitude of Eiffel Tower: 48.858370
+var firstname=document.getElementById("firstname");
+var person=null;
+var towerLat = 48.858370;
+var towerLon = 2.294481;
+var minDifferenceLat =Math.abs(local_data[0].latitude - towerLat);
+var minDifferenceLon = Math.abs(local_data[0].longitude - towerLon);
+
+for (var i = 0; i < local_data.length; i++) {
+  diffLat = Math.abs(local_data[i].latitude - towerLat);
+  diffLon = Math.abs(local_data[i].longitude -towerLon);
+  if (diffLat<minDifferenceLat && diffLon<minDifferenceLon ){
+    person = local_data[i];
+  }        
+};
+firstname.innerHTML = person.name.first;
 
 
  
